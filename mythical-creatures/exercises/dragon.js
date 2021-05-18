87e8f746c320bc107b400034bb17996078fe8877
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-05-17 21:39:48 -0500

    Pass dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,20 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.meals = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    if (this.meals < 2) {
+      this.meals += 1;
+    } else {
+      this.hungry = false;
+    }
+  }
+}
 
+module.exports = Dragon;
